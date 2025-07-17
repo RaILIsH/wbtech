9c304dd3b8c0afac26bbc481cc854966932cc357
Раиль Ишмуратов <railish@MacBook-Pro-Rail.local>
2025-07-18 00:40:37 +0300

    Test

--- L1/2/main.go
+++ L1/2/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	numbers := []int{2, 4, 6, 8, 10}
 	var wg sync.WaitGroup
-
+	// Возведение в квадрат с использованием WaitGroup для синхронизации горутин
 	for _, num := range numbers {
 		wg.Add(1)
 		go func(n int) {
